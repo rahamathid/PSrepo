8ea2bb118b937861ad64eaa9a577a19e883e8e30
rahamathid <rahamathid@yahoo.com>
2016-01-16 12:18:11 +0800

    added PS code

---
+++ PScode/CPU-Memory.ps1
@@ -0,0 +1,30 @@
+﻿   
+$Servers = gc .\host.txt
+"computername|DevideID|Name" | Out-file -Append .\Proc.txt
+"ComputerName|BankLabel|Capacity|Speed|DeviceLocator|SerialNumber" | out-file -Append .\Memory.txt
+
+Foreach ($ComputerName in $Servers) {
+    If (Test-Connection $ComputerName -Count 2 -Quiet) {
+        Write-Host "Working on : $ComputerName"
+        Try {
+            $Procs= Get-WmiObject Win32_processor -ComputerName $ComputerName -ErrorAction stop
+            Foreach ($Proc in $Procs){
+                "$computername|$($proc.DevideID)|$($Proc.Name)" | Out-file -Append .\Proc.txt
+            }
+            $Memorys = Get-WmiObject Win32_PhysicalMemory -ComputerName $ComputerName -ErrorAction stop
+            Foreach ($Memory in $Memorys) {
+                "$ComputerName|$($Memory.BankLabel)|$($Memory.Capacity)|$($Memory.Speed)|$($Memory.DeviceLocator)|$($Memory.SerialNumber)" | out-file -Append .\Memory.txt
+            }
+
+        }
+        Catch {
+            "$ComputerName : WMI Connection Failure" | Out-File -Append .\error.txt
+            Write-host "$ComputerName : WMI Connection Failure"
+        }
+
+    }
+    Else {
+        "$ComputerName :Ping Failed" | Out-File -Append .\Error.txt
+        Write-Host "$ComputerName :Ping Failed"
+    }
+}    
---
+++ PScode/Disable-Service.ps1
@@ -0,0 +1,30 @@
+﻿$service = "wuauserv" 
+$Servers = Get-Content -path .\host.txt
+foreach ($server in $Servers) {
+
+    write-host "Working on : " $Server
+    If (Test-Connection -ComputerName $server -Count 2 -Quiet) {
+        Try {
+            $stop = (gwmi win32_service -computername $server -filter "name='$service'" -ErrorAction stop).stopservice()
+            $result = (gwmi win32_service -computername $server -filter "name='$service'" -ErrorAction stop).ChangeStartMode("Disabled")
+            write-host "Disabled service on : " $Server
+            "Disabled and stopped service on : $Server" | Out-file -Append .\output.txt
+        }
+        Catch {
+            "Unable to Connect to WMI : $server" | Out-File -Append .\error.txt
+            Write-Host "Unable to Connect to WMI : $server"
+        }
+    
+    }
+    Else {
+        "Unable to ping :  $Server" | out-file -Append .\error.txt
+        Write-Host "Unable to ping : " $Server
+    }
+}
+
+$Server=""
+
+foreach ($Server in $servers) {
+    Get-Service -ComputerName $Server -Name $service
+
+}
